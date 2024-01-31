0165d668ef7e4a41be24824ffe427ed661ef8c42
NeutronNerd <116937508+NeutronJava@users.noreply.github.com>
2024-01-31 14:40:36 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-console.log('This is a popup!');
+var cusid_ele = document.getElementsByClassName('planner-course-lesson__description__cards-container__activity-cards');
+for (var i = 0; i < cusid_ele.length; ++i) {
+    var item = cusid_ele[i];  
+    chrome.runtime.sendMessage({value: String(item)}, null);
+}
